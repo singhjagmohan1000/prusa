5fdb8aef4ac4d679329897e248c31620074b79a5
jagmohansingh <jagmohan.singh@whitehatsec.com>
2019-02-28 20:54:15 -0800

    schedule reducer

--- src/actions/schedule.actions.js
+++ src/actions/schedule.actions.js
@@ -1,8 +1,79 @@
 import { SET_SCHEDULE_MONDAY } from "../constants/index";
-export function getData() {
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(json => {
-            return { type: "DATA_LOADED", payload: json };
-        });
+import { SET_SCHEDULE_TUESDAY } from "../constants/index";
+import { SET_SCHEDULE_WEDNESDAY } from "../constants/index";
+import { SET_SCHEDULE_THURSDAY } from "../constants/index";
+import { SET_SCHEDULE_FRIDAY } from "../constants/index";
+import { SET_SCHEDULE_SATURDAY } from "../constants/index";
+import { SET_SCHEDULE_SUNDAY } from "../constants/index";
+import axios from 'axios';
+const url = process.env.NODE_ENV === 'production' ? "/api/" : "localhost:8080/api/";
+
+export function scheduleMonday() {
+    return function(dispatch) {
+        return axios.get(`${url}scheduleMonday`)
+            .then(response => response.json())
+            .then(json => {
+                return { type: SET_SCHEDULE_MONDAY, payload: json };
+            }).catch((err)=>{console.log(err)});
+    };
+}
+
+export function scheduleTuesday() {
+    return function(dispatch) {
+        return axios.get(`${url}scheduleTuesday`)
+            .then(response => response.json())
+            .then(json => {
+                return { type: SET_SCHEDULE_TUESDAY, payload: json };
+            }).catch((err)=>{console.log(err)});
+    };
+}
+
+export function scheduleWednesday() {
+    return function(dispatch) {
+        return axios.get(`${url}scheduleWednesday`)
+            .then(response => response.json())
+            .then(json => {
+                return { type: SET_SCHEDULE_WEDNESDAY, payload: json };
+            }).catch((err)=>{console.log(err)});
+    };
+}
+
+export function scheduleThursday() {
+    return function(dispatch) {
+        return axios.get(`${url}scheduleThursday`)
+            .then(response => response.json())
+            .then(json => {
+                return { type: SET_SCHEDULE_THURSDAY, payload: json };
+            }).catch((err)=>{console.log(err)});
+    };
+}
+
+export function scheduleFriday() {
+    return function(dispatch) {
+        return axios.get(`${url}scheduleFriday`)
+            .then(response => response.json())
+            .then(json => {
+                return { type: SET_SCHEDULE_FRIDAY, payload: json };
+            }).catch((err)=>{console.log(err)});
+    };
+}
+
+export function scheduleSaturday() {
+    return function(dispatch) {
+        return axios.get(`${url}scheduleSaturday`)
+            .then(response => response.json())
+            .then(json => {
+                return { type: SET_SCHEDULE_SATURDAY, payload: json };
+            }).catch((err)=>{console.log(err)});
+    };
+}
+
+export function scheduleSunday() {
+    return function(dispatch) {
+        return axios.get(`${url}scheduleSunday`)
+            .then(response => response.json())
+            .then(json => {
+                return { type: SET_SCHEDULE_SUNDAY, payload: json };
+            }).catch((err)=>{console.log(err)});
+    };
 }
---
+++ src/reducers/scheduleReducer.js
@@ -0,0 +1,50 @@
+import { SET_SCHEDULE_MONDAY } from "../constants/index";
+import { SET_SCHEDULE_TUESDAY } from "../constants/index";
+import { SET_SCHEDULE_WEDNESDAY } from "../constants/index";
+import { SET_SCHEDULE_THURSDAY } from "../constants/index";
+import { SET_SCHEDULE_FRIDAY } from "../constants/index";
+import { SET_SCHEDULE_SATURDAY } from "../constants/index";
+import { SET_SCHEDULE_SUNDAY } from "../constants/index";
+const initialState = {
+    schedule_monday: [],
+    schedule_tuesday: [],
+    schedule_wednesday: [],
+    schedule_thursday: [],
+    schedule_friday: [],
+    schedule_saturday: [],
+    schedule_sunday: []
+};
+export default function footerReducer(state = initialState, action) {
+    switch (action.type) {
+        case SET_SCHEDULE_MONDAY:
+            return Object.assign({}, state, {
+                schedule_monday: state.schedule_monday.concat(action.payload)
+            });
+        case SET_SCHEDULE_TUESDAY:
+            return Object.assign({}, state, {
+                schedule_tuesday: state.schedule_tuesday.concat(action.payload)
+            });
+        case SET_SCHEDULE_WEDNESDAY:
+            return Object.assign({}, state, {
+                schedule_wednesday: state.schedule_wednesday.concat(action.payload)
+            });
+        case SET_SCHEDULE_THURSDAY:
+            return Object.assign({}, state, {
+                schedule_thursday: state.schedule_thursday.concat(action.payload)
+            });
+        case SET_SCHEDULE_FRIDAY:
+            return Object.assign({}, state, {
+                schedule_friday: state.schedule_friday.concat(action.payload)
+            });
+        case SET_SCHEDULE_SATURDAY:
+            return Object.assign({}, state, {
+                schedule_saturday: state.schedule_saturday.concat(action.payload)
+            });
+        case SET_SCHEDULE_SUNDAY:
+            return Object.assign({}, state, {
+                schedule_sunday: state.schedule_sunday.concat(action.payload)
+            });
+        default:
+            return state;
+    }
+}
